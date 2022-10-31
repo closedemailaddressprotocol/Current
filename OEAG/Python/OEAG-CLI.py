71bbb6cfa7f4331e335aabc1f52fa48ab662be6e
Stephen Martin <unixtown2002@outlook.com>
2022-10-30 22:44:03 -0400

    Remove non-alpha characters

--- OEAG/Python/OEAG-CLI.py
+++ OEAG/Python/OEAG-CLI.py
@@ -7,11 +7,12 @@ if len(sys.argv) == 1:
 	print ("usage: " + sys.argv[0] + " <authorization phrase>");
 	exit();
 
-
 secret=sys.argv[1];
 
 secret=re.sub('[\r\n]','',secret);
 
 secret = hashlib.sha256(secret.encode('utf-8')).hexdigest();
+
+secret=re.sub('[^A-Za-z0-9]','',secret);
 
 print(secret);
